49d0d0d4d8cb09cf3519493fd02a811710a38234
Aptech Computer <mahicse28@gmail.com>
2020-07-20 04:18:44 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/demoactivityforresult/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.demoactivityforresult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        editText = findViewById(R.id.editTextId);
+        button = findViewById(R.id.buttonId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = editText.getText().toString();
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                intent.putExtra("key",value);
+                setResult(1,intent);
+                finish();
+            }
+        });
+    }
+}
